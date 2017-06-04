e97e40002c1364391a62a142116245976741087a
LENOVO-THINKPAD\Branislav Bily <branislav.bily@gmail.com>
2017-06-05 00:14:17 +0200

    PlaySketch Kinda working!

---
+++ PlaySketch/canvasy/Asteroid/sketch.js
@@ -0,0 +1,75 @@
+
+var ship;
+var asteroids = [];
+var lasers = [];
+
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
+
+function setup() {
+  createCanvas(600, 600);
+  ship = new Ship();
+  for (var i = 0; i < 5; i++) {
+    asteroids.push(new Asteroid());
+  }
+}
+
+function draw() {
+  background(0);
+
+  for (var i = 0; i < asteroids.length; i++) {
+    if (ship.hits(asteroids[i])) {
+      console.log('Ó nie');
+      ship.pos.x = width / 2;
+      ship.pos.y = height / 2;
+    }
+    asteroids[i].render();
+    asteroids[i].update();
+    asteroids[i].edges();
+  }
+
+  for (var i = lasers.length - 1; i >= 0; i--) {
+    lasers[i].render();
+    lasers[i].update();
+    if (lasers[i].offscreen()) {
+      lasers.splice(i, 1);
+    } else {
+      for (var j = asteroids.length - 1; j >= 0; j--) {
+        if (lasers[i].hits(asteroids[j])) {
+          if (asteroids[j].r > 10) {
+            var newAsteroids = asteroids[j].breakup();
+            asteroids = asteroids.concat(newAsteroids);
+          }
+          asteroids.splice(j, 1);
+          lasers.splice(i, 1);
+          break;
+        }
+      }
+    }
+  }
+  ship.render();
+  ship.turn();
+  ship.update();
+  ship.edges();
+}
+
+function keyReleased() {
+  ship.setRotation(0);
+  ship.boosting(false);
+}
+
+function keyPressed() {
+  if (key == ' ') {
+    lasers.push(new Laser(ship.pos, ship.heading));
+  } else if (keyCode == RIGHT_ARROW) {
+    ship.setRotation(0.1);
+  } else if (keyCode == LEFT_ARROW) {
+    ship.setRotation(-0.1);
+  } else if (keyCode == UP_ARROW) {
+    ship.boosting(true);
+  }
+}
